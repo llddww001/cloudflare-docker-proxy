ffa586372138bf3b72d19ab73602d164d4dabf3c
Li David <david.li@volvo.com>
2026-02-03 17:43:57 +0800

    bugfxi

--- src/index.js
+++ src/index.js
@@ -7,24 +7,24 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + env.CUSTOM_DOMAIN]: dockerHub,
+  ["quay." + env.CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr." + env.CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr." + env.CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s." + env.CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr." + env.CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith." + env.CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr." + env.CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + env.CUSTOM_DOMAIN]: dockerHub,
 };
 
 function routeByHosts(host) {
   if (host in routes) {
     return routes[host];
   }
-  if (MODE == "debug") {
+  if (env.MODE == "debug") {
     return TARGET_UPSTREAM;
   }
   return "";
@@ -155,7 +155,7 @@ async function fetchToken(wwwAuthenticate, scope, authorization) {
 
 function responseUnauthorized(url) {
   const headers = new(Headers);
-  if (MODE == "debug") {
+  if (env.MODE == "debug") {
     headers.set(
       "Www-Authenticate",
       `Bearer realm="http://${url.host}/v2/auth",service="cloudflare-docker-proxy"`
